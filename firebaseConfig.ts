3bb8c38c9ab103783dd8997d523d64018f6b9653
Fatih MUTLU <33759331+fathf3@users.noreply.github.com>
2025-11-19 23:28:34 +0100

    refactor: Update Firebase initialization
    Migrates Firebase initialization to use `firebase.initializeApp` for consistency and allows for potential future expansion of Firebase services. Also, ensures the main application script is correctly linked in `index.html`.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore
 export const firestore = getFirestore(app);
